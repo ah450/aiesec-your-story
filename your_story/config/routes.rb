b7548c484dd1dd279f7aef33c266f06bc5c49c90
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2015-08-16 15:13:22 +0200

    Defines routes

--- your_story/config/routes.rb
+++ your_story/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do 
     resources :participants do 
-      resources :stories
+      resources :stories, only: [:index, :create, :get]
+      resources :avatar, only: [:create, :get]
     end
+    resources :states, only: [:index, :show]
+    resources :local_chapters, only: [:index, :get]
+    resources :configurations, only: [:index]
+    resources :tokens, only: [:create]
   end
 end
