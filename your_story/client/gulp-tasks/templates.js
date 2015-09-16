814ece0163c17222e186e62cd05fafbd018af399
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2015-09-17 01:48:46 +0200

    Improves frontent build system

--- your_story/client/gulp-tasks/templates.js
+++ your_story/client/gulp-tasks/templates.js
@@ -3,23 +3,27 @@ var concat = require('gulp-concat');
 var templateCache = require('gulp-angular-templatecache');
 var minifyHtml = require('gulp-minify-html');
 
-var templateOptions = {
-   standalone: true,
-   module: 'aiesecTemplates'
-};
+function createModule(src) {
+  return src.pipe(templateCache('templates.js', {
+      standalone: true,
+      module: 'aiesecTemplates'
+    }));
+}
 
-gulp.task('templates', function() {
-  // Turns templates to html
-  return gulp.src('src/views/**/*.html')
-    .pipe(templateCache('templates.js', templateOptions))
-    .pipe(gulp.dest('./build/templates'));
-});
+function processTemplates() {
+  var src = gulp.src('src/views/**/*.html');
+  return createModule(src);
+}
 
-gulp.task('templates-min', function() {
-  return gulp.src('src/views/**/*.html')
-    .pipe(minifyHtml({
-      empty: true
-    }))
-    .pipe(templateCache('templates.min.js', templateOptions))
-    .pipe(gulp.dest('./build/temp/'));
-});
+function minifyTemplates() {
+  var src = gulp.src('src/views/**/*.html')
+  .pipe(minifyHtml({
+    empty: true
+  }));
+  return createModule(src);
+}
+
+
+
+exports.processTemplates = processTemplates;
+exports.minifyTemplates = minifyTemplates;
